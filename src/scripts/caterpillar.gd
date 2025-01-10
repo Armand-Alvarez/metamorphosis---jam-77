21ba241b6752a42d36fd7beacc8e42db6a920132
Armand Alvarez <aralvarez159@gmail.com>
2025-01-10 18:10:46 -0500

    Moved animation handling to its own function. Both input & animation handling are done every frame

--- src/scripts/caterpillar.gd
+++ src/scripts/caterpillar.gd
@@ -8,27 +8,45 @@ extends CharacterBody2D
 
 
 
+func _ready() -> void:
+	$NormalCollisionPolygon2D.disabled = false
+	$AttackCollisionShape2D.disabled = true
+
+
 func _process(delta: float) -> void:
-	movement(delta)
+	handle_input(delta)
+	handle_animations()
 
 
-func movement(delta: float) -> void:
+func handle_input(delta: float) -> void:
 	if Input.is_action_pressed("move_right"):
 		move_and_collide(Vector2.RIGHT * speed * delta)
-		_animation_sprite.play("move")
 	if Input.is_action_pressed("move_left"):
 		move_and_collide(Vector2.LEFT * speed * delta)
-		_animation_sprite.play("move")
 	if Input.is_action_pressed("move_up"):
 		move_and_collide(Vector2.UP * speed * delta)
-		_animation_sprite.play("move")
 	if Input.is_action_pressed("move_down"):
 		move_and_collide(Vector2.DOWN * speed * delta)
+
+
+func handle_animations() -> void:
+	var playing = _animation_sprite.animation
+	if _animation_sprite.is_playing() == false: playing = ""
+	if Input.is_action_pressed("move_right") and playing != "attack":
 		_animation_sprite.play("move")
-	if not Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_left") and playing != "attack":
+		_animation_sprite.play("move")
+	if Input.is_action_pressed("move_up") and playing != "attack":
+		_animation_sprite.play("move")
+	if Input.is_action_pressed("move_down") and playing != "attack":
+		_animation_sprite.play("move")
+	if not Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down") and playing != "attack":
 		_animation_sprite.play("idle")
 
 	if Input.is_action_just_pressed("move_right"):
-		$AnimatedSprite2D.flip_h = true
+		_animation_sprite.flip_h = true
 	if Input.is_action_just_pressed("move_left"):
-		$AnimatedSprite2D.flip_h = false
+		_animation_sprite.flip_h = false
+
+	if Input.is_action_just_pressed("attack"):
+		_animation_sprite.play("attack")
