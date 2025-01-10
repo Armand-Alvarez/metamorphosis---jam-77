b319f13abe4f699d575e1b2921f8e2be0a0ea596
Armand Alvarez <aralvarez159@gmail.com>
2025-01-10 17:27:04 -0500

    Add speed property and movement logic for caterpillar character

--- src/scripts/caterpillar.gd
+++ src/scripts/caterpillar.gd
@@ -1,1 +1,26 @@
+class_name Caterpillar
 extends CharacterBody2D
+
+
+@export var speed: int = 300
+
+
+
+func _process(delta: float) -> void:
+	movement(delta)
+
+
+func movement(delta: float) -> void:
+	if Input.is_action_pressed("move_right"):
+		move_and_collide(Vector2.RIGHT * speed * delta)
+	if Input.is_action_pressed("move_left"):
+		move_and_collide(Vector2.LEFT * speed * delta)
+	if Input.is_action_pressed("move_up"):
+		move_and_collide(Vector2.UP * speed * delta)
+	if Input.is_action_pressed("move_down"):
+		move_and_collide(Vector2.DOWN * speed * delta)
+
+	if Input.is_action_just_pressed("move_right"):
+		$Sprite2D.flip_h = true
+	if Input.is_action_just_pressed("move_left"):
+		$Sprite2D.flip_h = false
