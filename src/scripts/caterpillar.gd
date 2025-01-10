78e183f70b9b0e1a1ed4662760a7a90dcb456afb
Armand Alvarez <aralvarez159@gmail.com>
2025-01-10 17:41:40 -0500

    Add animations for caterpillar movement and idle states

--- src/scripts/caterpillar.gd
+++ src/scripts/caterpillar.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 
 @export var speed: int = 300
 
+@onready var _animation_sprite = $AnimatedSprite2D
+
 
 
 func _process(delta: float) -> void:
@@ -13,14 +15,20 @@ func _process(delta: float) -> void:
 func movement(delta: float) -> void:
 	if Input.is_action_pressed("move_right"):
 		move_and_collide(Vector2.RIGHT * speed * delta)
+		_animation_sprite.play("move")
 	if Input.is_action_pressed("move_left"):
 		move_and_collide(Vector2.LEFT * speed * delta)
+		_animation_sprite.play("move")
 	if Input.is_action_pressed("move_up"):
 		move_and_collide(Vector2.UP * speed * delta)
+		_animation_sprite.play("move")
 	if Input.is_action_pressed("move_down"):
 		move_and_collide(Vector2.DOWN * speed * delta)
+		_animation_sprite.play("move")
+	if not Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+		_animation_sprite.play("idle")
 
 	if Input.is_action_just_pressed("move_right"):
-		$Sprite2D.flip_h = true
+		$AnimatedSprite2D.flip_h = true
 	if Input.is_action_just_pressed("move_left"):
-		$Sprite2D.flip_h = false
+		$AnimatedSprite2D.flip_h = false
