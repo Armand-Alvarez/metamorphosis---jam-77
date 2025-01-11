1cb0aaae7c8847dfe84770f9bb2ea1371591856b
Armand Alvarez <aralvarez159@gmail.com>
2025-01-11 17:01:20 -0500

    Add damage indicator and attack area to black ant mob

--- src/scripts/enemies/mobs/black_ant.gd
+++ src/scripts/enemies/mobs/black_ant.gd
@@ -23,3 +23,19 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var direction_vector = marker.position - position
 	move_and_collide(direction_vector.normalized() * speed * delta)
+	if health <= 0:
+		queue_free()
+
+
+func take_damage(amount: int) -> void:
+	health -= amount
+	$DamageIndicator.visible = true
+	$DamageIndicator/VisibileTimer.start()
+
+
+func _on_visibile_timer_timeout() -> void:
+	$DamageIndicator.visible = false
+
+
+func _on_attack_area_body_entered(body: Node2D) -> void:
+	body.take_damage(damage)
