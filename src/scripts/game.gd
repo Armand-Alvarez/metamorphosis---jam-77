4cf0641d33daf83d45d7af50a3acbecdfc98fa50
Armand Alvarez <aralvarez159@gmail.com>
2025-01-11 15:51:31 -0500

    Add game script for handling mob spawning and health updates

---
+++ src/scripts/game.gd
@@ -0,0 +1,32 @@
+class_name Game
+extends Node2D
+
+
+const mobs = {
+	"black_ant": preload("res://src/scenes/enemies/mobs/black_ant.tscn")
+}
+
+
+func _ready() -> void:
+	set_up_signal_bus_connections()
+
+
+func _process(delta: float) -> void:
+	handle_input()
+
+
+func set_up_signal_bus_connections() -> void:
+	SignalBus.health_changed.connect(_on_health_changed)
+
+
+func _on_health_changed(amount: int) -> void:
+	print("health has chagned by ", amount)
+	$UI/HealthBar.value += amount
+
+
+func handle_input() -> void:
+	if Input.is_action_just_pressed("spawn_mob"):
+		var random_marker = $CharacterBody2D/AIAttackPoints.get_children(true).pick_random()
+		var mob = mobs.get("black_ant").instantiate()
+		mob.marker = random_marker
+		add_child(mob)
