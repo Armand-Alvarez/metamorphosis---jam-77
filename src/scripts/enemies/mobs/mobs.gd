721ae2c5ae3778cbb69a6e43fc28af241ca4139c
Armand Alvarez <aralvarez159@gmail.com>
2025-01-11 15:51:41 -0500

    Add Mobs resource script with attributes for name, health, attack damage, speed, and texture

---
+++ src/scripts/enemies/mobs/mobs.gd
@@ -0,0 +1,12 @@
+class_name Mobs
+extends Resource
+
+
+@export_category("Attributes")
+@export var mob_name: String = "Mob Name"
+@export var mob_health: int = 10
+@export var mob_attack_damage: int = 2
+@export var mob_speed: int = 100
+
+@export_category("Art")
+@export var texture: Texture2D
