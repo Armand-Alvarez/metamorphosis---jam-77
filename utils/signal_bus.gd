e14f0542a8553f16d26853f20741bfc8d3f98dd4
Armand Alvarez <aralvarez159@gmail.com>
2025-01-12 16:29:16 -0500

    Add signals for leaf dropping and picking up in signal bus

--- utils/signal_bus.gd
+++ utils/signal_bus.gd
@@ -8,3 +8,8 @@ signal health_changed(health: int)
 
 # Mob Signals
 signal spawn_mob(marker: Marker2D)
+signal drop_leaves(amount: int, location: Vector2)
+
+
+# Game Signals
+signal leaf_picked_up()
