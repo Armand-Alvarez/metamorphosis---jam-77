812e8999a7604efe7bc413e0622f98263fe9b4f4
Armand Alvarez <aralvarez159@gmail.com>
2025-01-12 18:27:10 -0500

    Rename HBoxContainer to LeavesHBoxContainer and update labels to reflect leaves owned in the menu

--- src/scripts/menu.gd
+++ src/scripts/menu.gd
@@ -22,6 +22,7 @@ func _prepare_cost_labels() -> void:
 	$UpgradesCanvasLayer/VBoxContainer/HealthHBoxContainer/Cost.text = str(upgrade_costs[upgrade_levels["health"]])
 	$UpgradesCanvasLayer/VBoxContainer/AttackSpeedHBoxContainer/Cost.text = str(upgrade_costs[upgrade_levels["attack_speed"]])
 	$UpgradesCanvasLayer/VBoxContainer/AttackDamageHBoxContainer/Cost.text = str(upgrade_costs[upgrade_levels["attack_damage"]])
+	$UpgradesCanvasLayer/LeavesHBoxContainer/LeavesOwnedLabelLabel.text = str(get_parent().leaves_owned)
 
 
 func _on_start_button_pressed() -> void:
@@ -34,14 +35,17 @@ func _on_quit_button_pressed() -> void:
 
 func _on_health_upgrade_button_pressed() -> void:
 	SignalBus.upgrade_bought.emit("health")
+	_prepare_cost_labels()
 
 
 func _on_attack_damage_button_pressed() -> void:
 	SignalBus.upgrade_bought.emit("attack_damage")
+	_prepare_cost_labels()
 
 
 func _on_attack_speed_button_pressed() -> void:
 	SignalBus.upgrade_bought.emit("attack_speed")
+	_prepare_cost_labels()
 
 
 func _on_upgrade_back_button_pressed() -> void:
