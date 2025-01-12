83a9b2214bbe4ba231a7265d2fd08d02d4d5e185
Armand Alvarez <aralvarez159@gmail.com>
2025-01-12 16:29:01 -0500

    Add UI elements for leaves count and setup signal bus connections for leaf interactions

--- src/scripts/game.gd
+++ src/scripts/game.gd
@@ -5,12 +5,21 @@ extends Node2D
 const mobs = {
 	"black_ant": preload("res://src/scenes/enemies/mobs/black_ant.tscn")
 }
+@onready var leaf = preload("res://src/scenes/leaf.tscn")
+@export var leaves: int = 0
 
+
+func _ready() -> void:
+	set_up_signal_bus_connections()
+	leaves = 0
 
 
 func _process(_delta: float) -> void:
 	handle_input()
 
+func set_up_signal_bus_connections() -> void:
+	SignalBus.drop_leaves.connect(_on_leaves_dropped)
+	SignalBus.leaf_picked_up.connect(_on_leaf_picked_up)
 
 func handle_input() -> void:
 	if Input.is_action_just_pressed("spawn_mob"):
@@ -18,3 +27,15 @@ func handle_input() -> void:
 		var mob = mobs.get("black_ant").instantiate()
 		mob.marker = random_marker
 		add_child(mob)
+
+
+func _on_leaf_picked_up() -> void:
+	leaves += 1
+	$UI/CanvasLayer/HBoxContainer/LeavesCount.text = str(leaves)
+
+
+func _on_leaves_dropped(amount: int, location: Vector2) -> void:
+	for l in range(amount):
+		var temp_leaf = leaf.instantiate()
+		temp_leaf.position = location
+		add_child(temp_leaf)
