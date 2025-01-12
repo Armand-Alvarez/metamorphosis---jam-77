a8450a6eca99e7e95479f556a76efa02e2dd2b5c
Armand Alvarez <aralvarez159@gmail.com>
2025-01-12 18:11:27 -0500

    Add menu scene and script for game navigation and upgrades management

---
+++ src/scripts/menu.gd
@@ -0,0 +1,54 @@
+extends Control
+
+
+var upgrade_costs = []
+var upgrade_levels = {
+	"health": 0,
+	"attack_speed": 0,
+	"attack_damage": 0,
+}
+
+
+func _ready() -> void:
+	$MenuCanvasLayer.visible = true
+	$UpgradesCanvasLayer.visible = false
+	upgrade_costs = get_parent().upgrade_costs
+	upgrade_levels = get_parent().upgrade_levels
+	_prepare_cost_labels()
+
+
+
+func _prepare_cost_labels() -> void:
+	$UpgradesCanvasLayer/VBoxContainer/HealthHBoxContainer/Cost.text = str(upgrade_costs[upgrade_levels["health"]])
+	$UpgradesCanvasLayer/VBoxContainer/AttackSpeedHBoxContainer/Cost.text = str(upgrade_costs[upgrade_levels["attack_speed"]])
+	$UpgradesCanvasLayer/VBoxContainer/AttackDamageHBoxContainer/Cost.text = str(upgrade_costs[upgrade_levels["attack_damage"]])
+
+
+func _on_start_button_pressed() -> void:
+	SignalBus.start_button_pressed.emit()
+
+
+func _on_quit_button_pressed() -> void:
+	SignalBus.quit_button_pressed.emit()
+
+
+func _on_health_upgrade_button_pressed() -> void:
+	SignalBus.upgrade_bought.emit("health")
+
+
+func _on_attack_damage_button_pressed() -> void:
+	SignalBus.upgrade_bought.emit("attack_damage")
+
+
+func _on_attack_speed_button_pressed() -> void:
+	SignalBus.upgrade_bought.emit("attack_speed")
+
+
+func _on_upgrade_back_button_pressed() -> void:
+	$MenuCanvasLayer.visible = true
+	$UpgradesCanvasLayer.visible = false
+
+
+func _on_upgrades_button_pressed() -> void:
+	$MenuCanvasLayer.visible = false
+	$UpgradesCanvasLayer.visible = true
